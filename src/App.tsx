feb16fedd2878781faebbd49ed6c5d876e73e244
daniil-kaverzin <daniil.kaverzin@outlook.com>
2021-11-06 18:07:32 +0700

    fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import React, {FC} from 'react';
-import Dropdown from "./components/Dropdown";
+import { useEffect } from 'react';
+import {FC, useState} from 'react';
 import styled from "styled-components";
+
+import Dropdown from "./components/Dropdown";
+import { Value } from './interfaces';
 
 const options = [
   {value: 1, label: 'test'},
@@ -18,9 +21,30 @@ const Wrapper = styled.div`
 `
 
 const App: FC = () => {
+  const [firstValue, setFirstValue] = useState<Value>();
+  const [secondValue, setSecondValue] = useState<Value>(2);
+
+  useEffect(() => {
+    console.log('First:', firstValue);
+  }, [firstValue])
+  useEffect(() => {
+    console.log('Second:', secondValue);
+  }, [secondValue])
+
   return (
     <Wrapper>
-      <Dropdown options={options} placeholder={'sasi'}/>
+      <Dropdown
+        options={options}
+        selected={firstValue}
+        onSelectedChange={setFirstValue}
+      />
+      <Dropdown
+        style={{ marginLeft: 16 }}
+        options={options}
+        placeholder="sam sasi"
+        selected={secondValue}
+        onSelectedChange={setSecondValue}
+      />
     </Wrapper>
   );
 }
